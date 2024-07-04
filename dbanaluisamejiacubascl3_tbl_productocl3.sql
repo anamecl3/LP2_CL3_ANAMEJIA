ff657d6e26fd39b4181162164514afa371dfe153
ANA LUISA MEJIA CUBAS <luisacrazy81@gmail.com>
2024-07-04 14:42:07 -0500

    BASE DE DATOS CL3

---
+++ dbanaluisamejiacubascl3_tbl_productocl3.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: BDANALUISAMEJIACUBASCL3
+-- ------------------------------------------------------
+-- Server version	5.5.24-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_productocl3`
+--
+
+DROP TABLE IF EXISTS `tbl_productocl3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_productocl3` (
+  `idproductocl3` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcl3` varchar(255) DEFAULT NULL,
+  `estadocl3` varchar(255) DEFAULT NULL,
+  `nombrecl3` varchar(255) DEFAULT NULL,
+  `preciocompcl3` double DEFAULT NULL,
+  `precioventacl3` double DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idproductocl3`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_productocl3`
+--
+
+LOCK TABLES `tbl_productocl3` WRITE;
+/*!40000 ALTER TABLE `tbl_productocl3` DISABLE KEYS */;
+INSERT INTO `tbl_productocl3` VALUES (1,'2018','ACTIVO','IPHONE 11',1500,2299,10),(2,'2019','ACTIVO','IPHONE 12',2999,3320,50),(3,'2020','ACTIVO','IPHONE 13',3000,4500,5),(4,'SN','ACTIVO','IPHONE 20',2000,5000,10);
+/*!40000 ALTER TABLE `tbl_productocl3` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-04 13:59:22
